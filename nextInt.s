bb5ec9a4e6e76804b9748799822d2b7b7c0a5dee
JoseAtaQ <jose.ata-quiroz.381@my.csun.edu>
2023-11-11 15:58:03 -0800

    MIPS wrong

--- nextInt.s
+++ nextInt.s
@@ -24,34 +24,34 @@ glyph2int:      nop                     #public static int glyph2int(char glyph,
                 li $t2, 0
                 li $t3, 0
 
-nitA:           nop                     #;
-                blt $t0, 'A'            #if (glyph >= 'A'){
+initA:           nop                    #;
+                blt $t0, 'A' forwardA   #if (glyph >= 'A'){
         initF:          nop             #        ;      
-                        bgt $t0, 'F'    #        if (glyph <= 'F'){
+                        bgt $t0, 'F' forwadF   # if (glyph <= 'F'){
 consA:          nop                     #;          
                                 sub $t3, $t0, 'A' #        c = glyph - 'A'
-                                addi $t3, 10      #        c = c + 10;
+                                addi $t3, $t3, 10 #        c = c + 10;
                                         #        }
         forwadF:        nop             #        ;       
                                         #}
 forwardA:       nop                     #;
 
 inita:          nop                     #;         
-                blt $t0, 'a'            #if (glyph >= 'a'){
+                blt $t0, 'a' forwada    #if (glyph >= 'a'){
         initf:          nop             #        ;      
-                        bgt $t0, 'f'    #        if (glyph <= 'f'){
+                        bgt $t0, 'f' forwadf   # if (glyph <= 'f'){
 consa:          nop                     #;                
                                 sub $t3, $t0, 'A' #        c = glyph - 'a'
-                                addi $t3, 10      #        c = c + 10;
+                                addi $t3, $t3, 10 #        c = c + 10;
                                         #        }
         forwadf:        nop             #        ;            
                                         #} 
 forwada:        nop                     #;
 
 init0:          nop                     #;            
-                blt $t0, '0'            #if (glyph >= '0'){
+                blt $t0, '0' forwad0    #if (glyph >= '0'){
         init9:          nop             #        ;      
-                        bgt $t0, '9'    #        if (glyph <= '9'){
+                        bgt $t0, '9' forwad9   # if (glyph <= '9'){
 cons0:          nop                     #;                
                                 sub $t3, $t0, '0'#       c = glyph - '0';
                                         #        }
@@ -83,25 +83,32 @@ nextInt:        nop                     #public static int nextInt(int radix){
                 li $t8, 256             #mips.read_s(buffer, 256);
                 la $t9, buffer          #$t9: buffer
                 read_s($t9, $t8)
-                                        #buffer_length = mips.retval();    
-            #i = 0;
-fnit:       ;
-loop:       for (; buffer[i] != '\0';){       
-body:           ;
-                value = glyph2int(buffer[i], radix);// gives me the value of a glyph following the index
-        initr:  ;      
-                if (value != -1){
-        consr:      ;   
-                    r = r * radix;
-                    r = r + value;
-                }
-        forwadr:;
-                i ++;
-                continue loop;
-            }// end for
-done:       ;       
-    return r;
-}// end nextInt
+                                        #buffer_length = mips.retval();
+                la $t2, buffer          #$t3: buffer[i]       
+                add $t2, $t2, $t5
+                lb $t3, 0($t2) 
+
+                li $t5, 0               #i = 0;
+fnit:           nop                     #;
+
+loop:           beq $t3, '\0' done      #for (; buffer[i] != '\0';){       
+body:           nop                     #;
+                call glyph2int $t3 $t1  #       value = glyph2int(buffer[i], radix);
+                move $t7, $v0
+        initr:          nop             #       ;      
+                        beq $t7, -1 forwadr #   if (value != -1){
+        consr:          nop             #       ;   
+                                mul $t6, $t6, $t1  #  r = r * radix;
+                                add $t6, $t6, $t7  #  r = r + value;
+                                        #       }
+        forwadr:        nop             #       ;
+                addi $t5, $t5, 1        # i++;
+                b loop                  # continue loop
+                                        # }// end for
+done:           nop                     # ;       
+        move $v0, $t6                   # return r;
+        jr $ra
+                                        #}// end nextInt
 
 
 #// java complete: Fri Nov 10 15:27:52 PST 2023
